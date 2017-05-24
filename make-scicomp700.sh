122616f89d12ebb343fb30d2edc902bf5dcc29c6
SciCompUser1 <scicomp@bnl.gov>
2017-05-24 16:25:59 -0400

    Add lab1

--- make-scicomp700.sh
+++ make-scicomp700.sh
@@ -3,5 +3,4 @@ mkdir ~/scicomp-labs-cpp-linux
 git clone https://github.com/dbiersach/scicomp-labs-cpp-linux.git ~/scicomp-labs-cpp-linux
 mkdir ~/Desktop/scicomp700
 git clone https://github.com/dbiersach/scicomp700-cpp-linux.git ~/Desktop/scicomp700
-
-
+cp -ar ~/scicomp-labs-cpp-linux/uniform-variance/ ~/Desktop/scicomp700/session01/lab1/

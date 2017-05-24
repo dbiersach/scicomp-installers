5100b0ced1f53f7c621f3a16394dc04f6e6bd38f
SciCompUser1 <scicomp@bnl.gov>
2017-05-24 16:07:53 -0400

    Add scicomp700

---
+++ make-scicomp700.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir ~/scicomp-labs-cpp-linux
+git clone https://github.com/dbiersach/scicomp-labs-cpp-linux.git ~/scicomp-labs-cpp-linux
+mkdir ~/Desktop/scicomp700
+git clone https://github.com/dbiersach/scicomp700-cpp-linux.git ~/Desktop/scicomp700
+
+

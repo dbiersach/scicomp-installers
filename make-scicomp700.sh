ddb1777e64f266643f815dc34cea888e5dfb2085
SciCompUser1 <scicomp@bnl.gov>
2017-05-25 01:14:55 -0400

    Add session02

--- make-scicomp700.sh
+++ make-scicomp700.sh
@@ -4,3 +4,8 @@ git clone https://github.com/dbiersach/scicomp-labs-cpp-linux.git ~/scicomp-labs
 mkdir ~/Desktop/scicomp700
 git clone https://github.com/dbiersach/scicomp700-cpp-linux.git ~/Desktop/scicomp700
 cp -ar ~/scicomp-labs-cpp-linux/uniform-variance/ ~/Desktop/scicomp700/session01/lab1/
+cp -ar ~/scicomp-labs-cpp-linux/lrss-bubble/ ~/Desktop/scicomp700/session02/lab1/
+cp -ar ~/scicomp-labs-cpp-linux/lrss-qsort/ ~/Desktop/scicomp700/session02/lab2/
+cp -ar ~/scicomp-labs-cpp-linux/freq-substr/ ~/Desktop/scicomp700/session02/lab3/
+cp -ar ~/scicomp-labs-cpp-linux/stoichiometry-instructor/ ~/Desktop/scicomp700/session02/lab4/
+

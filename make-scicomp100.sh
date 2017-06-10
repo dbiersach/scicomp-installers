868e53997131b7c945d69b1c1a195d94ec625b14
SciCompUser1 <scicomp@bnl.gov>
2017-06-10 18:46:41 -0400

    Add session04

---
+++ make-scicomp100.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mkdir ~/scicomp-labs-cpp-linux
+git clone https://github.com/dbiersach/scicomp-labs-cpp-linux.git ~/scicomp-labs-cpp-linux
+mkdir ~/Desktop/scicomp100
+git clone https://github.com/dbiersach/scicomp100-cpp-linux.git ~/Desktop/scicomp100
+cp -ar ~/scicomp-labs-cpp-linux/hello-world/ ~/Desktop/scicomp100/session03/lab1/
+cp -ar ~/scicomp-labs-cpp-linux/age-converter/ ~/Desktop/scicomp100/session04/lab1/
+cp -ar ~/scicomp-labs-cpp-linux/temperature-converter/ ~/Desktop/scicomp100/session04/lab2/

592a164f8c0dec9291cf94e84f998adaa6534f8b
SciCompUser1 <scicomp@bnl.gov>
2017-06-19 19:28:23 -0400

    Change session order

--- make-scicomp100.sh
+++ make-scicomp100.sh
@@ -5,10 +5,11 @@ git clone https://github.com/dbiersach/scicomp-labs-cpp-linux.git ~/scicomp-labs
 rm -f -r ~/Desktop/scicomp100/
 mkdir ~/Desktop/scicomp100
 git clone https://github.com/dbiersach/scicomp100-cpp-linux.git ~/Desktop/scicomp100
-cp -ar ~/scicomp-labs-cpp-linux/circuits-logisim/ ~/Desktop/scicomp100/session02/lab1/
-cp -ar ~/scicomp-labs-cpp-linux/hello-world/ ~/Desktop/scicomp100/session03/lab1/
-cp -ar ~/scicomp-labs-cpp-linux/age-converter/ ~/Desktop/scicomp100/session04/lab1/
-cp -ar ~/scicomp-labs-cpp-linux/temperature-converter/ ~/Desktop/scicomp100/session04/lab2/
+cp -ar ~/scicomp-labs-cpp-linux/hello-world/ ~/Desktop/scicomp100/session02/lab1/
+cp -ar ~/scicomp-labs-cpp-linux/age-converter/ ~/Desktop/scicomp100/session03/lab1/
+cp -ar ~/scicomp-labs-cpp-linux/temperature-converter/ ~/Desktop/scicomp100/session03/lab2/
+cp -ar ~/scicomp-labs-cpp-linux/circuits-logisim/ ~/Desktop/scicomp100/session04/lab2/
+cp -ar ~/scicomp-labs-cpp-linux/circuits-logisim/ ~/Desktop/scicomp100/session04/lab3/
 cp -ar ~/scicomp-labs-cpp-linux/gauss-sum/ ~/Desktop/scicomp100/session05/lab1/
 cp -ar ~/scicomp-labs-cpp-linux/jenga-14/ ~/Desktop/scicomp100/session05/lab2/
 cp -ar ~/scicomp-labs-cpp-linux/jenga-15/ ~/Desktop/scicomp100/session05/lab3/

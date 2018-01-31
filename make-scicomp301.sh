e0479ccdd3784ebf6c75e48e5c4c4216946a0beb
scicompusera <scicomp@bnl.gov>
2018-01-30 19:55:40 -0500

    Add Session folders

--- make-scicomp301.sh
+++ make-scicomp301.sh
@@ -2,12 +2,9 @@
 rm -f -r ~/scicomp-labs-cpp-linux/
 mkdir ~/scicomp-labs-cpp-linux
 git clone https://github.com/dbiersach/scicomp-labs-cpp-linux.git ~/scicomp-labs-cpp-linux
-#rm -f -r ~/Desktop/scicomp301/
-#mkdir ~/Desktop/scicomp301
-#git clone https://github.com/dbiersach/scicomp301-cpp-linux.git ~/Desktop/scicomp301
-cd ~/Desktop/scicomp301
-git stash
-git pull
+rm -f -r ~/Desktop/scicomp301/
+mkdir ~/Desktop/scicomp301
+git clone https://github.com/dbiersach/scicomp301-cpp-linux.git ~/Desktop/scicomp301
 cp -ar ~/scicomp-labs-cpp-linux/hello-world/ ~/Desktop/scicomp301/session02/lab1/
 cp -ar ~/scicomp-labs-cpp-linux/age-converter/ ~/Desktop/scicomp301/session02/lab2/
 cp -ar ~/scicomp-labs-cpp-linux/temperature-converter/ ~/Desktop/scicomp301/session02/lab3/
@@ -22,6 +19,6 @@ cp -ar ~/scicomp-labs-cpp-linux/dealer-bogus/ ~/Desktop/scicomp301/session04/lab
 cp -ar ~/scicomp-labs-cpp-linux/dealer-slow/ ~/Desktop/scicomp301/session04/lab3/
 cp -ar ~/scicomp-labs-cpp-linux/dealer-fast/ ~/Desktop/scicomp301/session04/lab4/
 cp -ar ~/scicomp-labs-cpp-linux/primality-race-v1/ ~/Desktop/scicomp301/session04/lab5/
-git stash pop
+
 
 
